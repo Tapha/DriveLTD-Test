816d2faf99a955b3917ee078cccdce4ba0e0fad1
Tapha Ngum <tapha@live.co.uk>
2011-07-11 14:35:49 +0300

    new thing

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://driveltd.com/';
+$config['base_url']	= 'http://driveltd.fluxflex.com/';
 
 /*
 |--------------------------------------------------------------------------
